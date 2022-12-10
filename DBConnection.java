49ba37d61d9b0db6a69f2f33c5802491eaeae2e0
Kumar1245 <108192196+Kumar1245@users.noreply.github.com>
2022-12-10 18:10:34 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DB;
+import java.sql.*;
+public class DBConnection {
+    public Connection con;
+    public Statement stmt;
+    public PreparedStatement pstmt;
+    public ResultSet rst;
+    
+
+/**
+ *
+ * @author HP
+ */
+    public DBConnection(){
+     try
+     {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy_strore","root","root");
+        }
+        catch(Exception e)
+                {
+                e.printStackTrace();
+        }
+    } 
+}
+    
+    
+   
+    
+
+    
+
